83fb9a1e30eb2e012288b3edd42d48b3dabf1f40
Matthew Abbott <abbottmg@me.com>
2025-07-09 16:50:40 -0500

    Pull in additional dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.jooq.jooq-codegen-gradle") version "3.20.5"
 }
 
 group = "codes.abbott"
@@ -20,10 +21,23 @@ repositories {
 }
 
 dependencies {
+	// jooq
+	implementation("org.jooq:jooq:3.20.5")
+	implementation("org.jooq:jooq-codegen-gradle:3.20.5")
+	implementation("org.jooq.jooq-codegen-gradle:org.jooq.jooq-codegen-gradle.gradle.plugin:3.20.5")
+	implementation("org.jooq:jooq-meta:3.20.5")
 	implementation("org.springframework.boot:spring-boot-starter-jooq")
+	// postgres
+	runtimeOnly("org.postgresql:postgresql")
+	jooqCodegen("org.postgresql:postgresql")
+	testImplementation("org.testcontainers:postgresql")
+	// liquibase
+	//implementation("org.liquibase:liquibase-core:4.32.0")
+	// spring boot general
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("org.postgresql:postgresql")
+	developmentOnly("org.springframework.boot:spring-boot-starter-actuator")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
