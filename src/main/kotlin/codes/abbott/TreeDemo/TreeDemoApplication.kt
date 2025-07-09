b457fb4a0d61c2fd4ca14904190cc28ab231c369
Matthew Abbott <abbottmg@me.com>
2025-07-09 16:59:00 -0500

    Implement the quickstart app controller

--- src/main/kotlin/codes/abbott/TreeDemo/TreeDemoApplication.kt
+++ src/main/kotlin/codes/abbott/TreeDemo/TreeDemoApplication.kt
@@ -2,10 +2,25 @@ package codes.abbott.TreeDemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
+@RestController
 @SpringBootApplication
-class TreeDemoApplication
+class TreeDemoApplication {
 
+	@RequestMapping("/hello")
+	fun hello(@RequestParam name: String = "World"): String {
+		return String.format("Hello %s!", name)
+	}
+
+	@GetMapping("/")
+	fun getBase(): String {
+		return "Hello, base!"
+	}
+}
 fun main(args: Array<String>) {
 	runApplication<TreeDemoApplication>(*args)
 }
