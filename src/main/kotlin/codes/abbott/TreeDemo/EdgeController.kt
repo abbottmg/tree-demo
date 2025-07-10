6bfc9f917411856baf067d3e9cdee65650b9b73b
Matthew Abbott <abbottmg@me.com>
2025-07-10 16:22:12 -0500

    Create EdgeController
    with a POST handler to create new edges

---
+++ src/main/kotlin/codes/abbott/TreeDemo/EdgeController.kt
@@ -0,0 +1,28 @@
+package codes.abbott.TreeDemo
+
+import codes.abbott.TreeDemo.db.public.tables.records.EdgeRecord
+import codes.abbott.TreeDemo.db.public.tables.references.EDGE
+import org.jooq.DSLContext
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/edge")
+class EdgeController(
+	@Autowired
+	var jooq: DSLContext
+) {
+
+	@PostMapping("/{from}/{to}")
+	fun createEdge(
+        @PathVariable from: Long,
+        @PathVariable to: Long
+	) {
+		val record = EdgeRecord(fromId = from, toId = to)
+		jooq.insertInto(EDGE).set(record).execute()
+	}
+}
