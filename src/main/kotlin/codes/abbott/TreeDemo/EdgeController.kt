a99f3d4cb6ad6bcd8e29d304161c60f1dc889eca
Matthew Abbott <abbottmg@me.com>
2025-07-10 16:59:20 -0500

    Add DELETE handler
    which always returns a list of zero or one items which were found and deleted.

--- src/main/kotlin/codes/abbott/TreeDemo/EdgeController.kt
+++ src/main/kotlin/codes/abbott/TreeDemo/EdgeController.kt
@@ -5,6 +5,7 @@ import codes.abbott.TreeDemo.db.public.tables.records.EdgeRecord
 import codes.abbott.TreeDemo.db.public.tables.references.EDGE
 import org.jooq.DSLContext
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -30,5 +31,19 @@ class EdgeController(
 	) {
 		val record = EdgeRecord(fromId = from, toId = to)
 		jooq.insertInto(EDGE).set(record).execute()
+	}
+
+
+	@DeleteMapping("/{from}/{to}")
+	fun deleteEdge(
+		@PathVariable from: Long,
+		@PathVariable to: Long
+	): List<Edge> {
+		val e = EDGE
+		return jooq.deleteFrom(e)
+			.where(EDGE.FROM_ID.eq(from))
+			.and(EDGE.TO_ID.eq(to))
+			.returning()
+			.fetchInto(Edge::class.java)
 	}
 }
